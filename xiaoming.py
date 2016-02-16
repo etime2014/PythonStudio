a0caa676a2505b8c60667b102e062484f4ecaa38
kuritan <yujia.zhai@hotmail.com>
2016-02-16 22:18:05 +0800

    Create xiaoming.py

---
+++ xiaoming.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+x = input("请输入小明去年的成绩:")
+s1 = int(x)
+y = input("请输入小明今年的成绩:")
+s2 = int(y)
+r = (s2-s1)/s1
+print("小明成绩提升了%.2f%%"%r)
+input("Press Enter to exit...")
