e738b63a8648c12e76ba1dc1d73933d9b7a4c148
kuritan <yujia.zhai@hotmail.com>
2016-07-06 20:45:42 +0800

    first python
    learn python hard way

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yag! Printing.'
+print "I'd like much rather you 'not'."
+print 'I "said" do not touch this.'
