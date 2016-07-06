4db1499708664bece2e92ee2246a05716ae12ef7
kuritan <yujia.zhai@hotmail.com>
2016-07-06 21:44:37 +0800

    5th
    hard way ex5

---
+++ ex5.py
@@ -0,0 +1,14 @@
+my_name = 'kuritan'
+my_age = 17 # a lie
+my_weight = 75 #kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d kg heavy." % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d , and %d I get %d." % (
+	my_age, my_weight, my_age + my_weight)
