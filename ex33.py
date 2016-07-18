be3c659d16591f1e768fd68db5d70f68e29ef61a
kuritan <yujia.zhai@hotmail.com>
2016-07-18 21:57:37 +0800

    7/18 first commit
    ex33 _1 _2

---
+++ ex33.py
@@ -0,0 +1,43 @@
+#i = 0
+#numbers = []
+
+#while i < 6:
+#	print "At the top i is %d" % i
+#	numbers.append(i)
+
+#	i = i + 1
+#	print "Numbers now: ", numbers
+#	print "At the bottom i is %d" % i
+
+
+#print "The numbers: "
+
+#for num in numbers:
+#	print num
+
+#from sys import argv
+
+#script, number_input = argv
+
+
+#numbers = []
+
+numbers = []
+def number_count(x):
+	"""This funtion will count the input number."""
+	i = 0
+	while i < x:
+		print "At the top i is %d" % i
+		numbers.append(i)
+
+		i = i + 1
+		print "Numbers now: ", numbers
+		print "At the bottom i is %d" % i
+
+number_input = raw_input(">Number_input ?")
+number_count(int(number_input))
+
+print "The numbers: "
+
+for num in numbers:
+	print num
