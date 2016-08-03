9cdf6900a1e8104b62d0deb25ba85fe336b77feb
kuritan <yujia.zhai@hotmail.com>
2016-08-03 21:43:52 +0800

    8/3 commit
    ex47,50

---
+++ projects/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = {
+	'description': 'My Project',
+	'author': 'kuritan',
+	'url': 'URL to get it',
+	'download_url': 'Where to download it.',
+	'author_email': 'lizi103@hotmail.com',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['NAME'],
+	'scripts': [],
+	'name': 'projectname'
+}
+
+setup(**config)
