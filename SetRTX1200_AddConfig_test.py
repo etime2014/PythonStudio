12ac73f14dbaaad928256e50481e2c34b55152c5
kuritan <yujia.zhai@hotmail.com>
2018-04-06 18:10:30 +0900

    change

--- SetRTX1200_AddConfig_test.py
+++ SetRTX1200_AddConfig_test.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+#こちらは、どこでも事務所ルータを設置する時のスクリプトです
+#実行するために、python2.X環境が必要
+#ルータ側は事前にSSH設定を済んで、lan3にIPを設定し、SSHリモート可能の状態に設置してください
+#ユーザ情報も手動でコンフィグを入れてください
+#sshd service on
+#sshd host key generate 2048
+#下記IPは例です
+#ip lan3 address 192.168.0.12/24
+#ip route 192.168.0.0/24 gateway 192.168.0.5
+#administrator password *
+#login user test *
+#事前準備のコンフィグ設定後、saveをお忘れなく
+
 import paramiko
 import time
 import getpass
 import ConfigParser
 
 config = ConfigParser.ConfigParser()
-config.read('./SetRTX1200_AddConfig_parameter')
+config.read('./SetRTX1200_AddConfig_parameter.ini')
 
-ip = config.get('General', 'ip_lan3')
+ssh_ip = config.get('General', 'ip_lan3')
 username = config.get('General', 'username')
 password = config.get('General', 'password')
-adminpw = getpass.getpass("administrator PW for %s: " % ip)
+adminpw = getpass.getpass("administrator PW for %s: " % ssh_ip)
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname=ip,username=username,password=password)
+ssh_client.connect(hostname=ssh_ip,username=username,password=password)
 
-print "Sucessfully login to ", ip
+print "Sucessfully login to ", ssh_ip
 
 command = ssh_client.invoke_shell()
 
@@ -92,20 +105,20 @@ command.send('ipsec sa policy ' + config.get('Tunnel', 'ipsec_tunnel_num') + ' '
 command.send('ipsec ike keepalive use ' + config.get('Tunnel', 'tunnel_num') + ' on icmp-echo ' + config.get('General', 'ip_router_hq') + ' 10 5' + '\n')
 time.sleep(0.5)
 #command.send("ipsec ike pre-shared-key 3 text Gaiasystem8811\n")
-command.send('ipsec ike pre-shared-key ' + config.get('Tunnel', 'tunnel_num') + config.get('Pre-shared-key', 'type') + ' ' + config.get('Pre-shared-key', 'content') + '\n')
+command.send('ipsec ike pre-shared-key ' + config.get('Tunnel', 'tunnel_num') + ' ' + config.get('Pre-shared-key', 'type') + ' ' + config.get('Pre-shared-key', 'content') + '\n')
 #command.send("ipsec ike remote address 3 121.1.133.74\n")
-command.send('ipsec ike remote address ' + config.get('Tunnel', 'tunnel_num') + config.get('Tunnel', 'remote_address') + '\n')
+command.send('ipsec ike remote address ' + config.get('Tunnel', 'tunnel_num') + ' ' + config.get('Tunnel', 'remote_address') + '\n')
 #command.send("tunnel enable 3\n")
 command.send('tunnel enable ' + config.get('Tunnel', 'tunnel_num') + '\n')
 #command.send("tunnel select none\n")
 command.send('tunnel select none' + '\n')
 time.sleep(0.5)
 #command.send("dhcp scope 1 172.25.32.21-172.25.32.28/24\n")
-command.send('dhcp scope ' + config.get('DHCP', 'scope_num') + config.get('DHCP', 'scope_range') + '\n')
+command.send('dhcp scope ' + config.get('DHCP', 'scope_num')  + ' ' + config.get('DHCP', 'scope_range') + '\n')
 #command.send("dhcp scope option 1 dns=172.31.102.210,172.31.102.143\n")
-command.send('dhcp scope option ' + config.get('DHCP', 'scope_num') + config.get('DHCP', 'dhcp_primary_dns') + ',' + config.get('DHCP', 'dhcp_secondary_dns') + '\n')
+command.send('dhcp scope option ' + config.get('DHCP', 'scope_num') + ' dns=' + config.get('DHCP', 'dhcp_primary_dns') + ',' + config.get('DHCP', 'dhcp_secondary_dns') + '\n')
 #command.send("dns server 202.224.32.1 202.224.32.2\n")
-command.send('dns server ' + config.get('General', 'net_primary_dns') + config.get('DHCP', 'net_secondary_dns') + '\n')
+command.send('dns server ' + config.get('General', 'net_primary_dns') + ' ' + config.get('General', 'net_secondary_dns') + '\n')
 #command.send("dns server pp 1\n")
 command.send('dns server pp ' + config.get('PP', 'pp_num') + '\n')
 time.sleep(0.5)
@@ -113,11 +126,12 @@ time.sleep(0.5)
 #command.send("no ip route 192.168.0.0/24 gateway 192.168.0.5\n")
 command.send('no ip route ' + config.get('General', 'default_route_intra') + '\n')
 #command.send("no ip lan3 address 192.168.0.12/24\n")
-command.send('no ip lan3 address ' + config.get('General', 'ip_lan3') + '\n')
+command.send('no ip lan3 address ' + config.get('General', 'ip_lan3') + config.get('General', 'ip_lan3_prefix')  + '\n')
 command.send("save\n")
-time.sleep(2.5)
+time.sleep(3)
 output = command.recv(65535)
 print output
+time.sleep(1)
 print "PP内local address未設定!!!"
 print "トンネル内ipsec ike local address未設定!!!"
 
